242aa735d0c5f929681b772d7957f2fa1291fc19
jiniusevil <jiniusevil@gmail.com>
2022-12-01 12:40:59 +0800

    said bye to pop os!

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ let change = () =>{
     const bigWords = document.getElementsByClassName("title");
     bigWords[0].style.color="green";
     bigWords[1].style.fontSize="20px";
-    console.log("Bye Apple!")
+    console.log("Bye Pop_os!")
 }
